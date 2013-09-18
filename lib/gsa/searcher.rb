88e149a77aad0c6176dd6fd7f42eafe01a8e4dc5
daniel-jerome-long <daniel@danieljlong.com>
2013-09-18 10:39:48 -0500

    searcher

---
+++ lib/gsa/searcher.rb
@@ -0,0 +1,32 @@
+module GSA
+  class Searcher
+    def self.search(query, args={})
+      query           = clean_query(query)
+      filter          = args[:filter]    || GSA::DEFAULT_FILTER
+      getfields       = args[:getfields] || GSA::DEFAULT_GETFIELDS
+      sort            = args[:sort]      || GSA::DEFAULT_SORT
+      num             = args[:num]       || GSA::DEFAULT_NUM  
+      output          = args[:output]    || GSA::DEFAULT_OUTPUT
+      requiredfields  = args[:filters]   || nil
+
+      search = "#{GSA::BASE_URI}/search?q=#{query}&filter=#{filter}&getfields=#{getfields}&sort=#{sort}&num=#{num}&output=#{output}"
+      search = "#{search}&requiredfields=#{clean_query(requiredfields)}" if requiredfields
+
+      search_results = parsed_json( RestClient.get(search) )
+
+      results_found?(search_results) ? search_results : GSA::NO_RESULTS
+    end
+
+    def self.clean_query(query)
+      query.tr(' ', '+')
+    end
+
+    def self.parsed_json(xml)
+      JSON.parse(Hash.from_xml(xml).to_json)
+    end
+
+    def self.results_found?(search_results)
+      search_results[GSA::GOOGLE_SEARCH_PROTOCOL].has_key? GSA::RESULTS
+    end
+  end
+end
