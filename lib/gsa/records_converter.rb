b4bdb10c2ff42cc4529802cf502512c01505efdc
daniel-jerome-long <daniel@danieljlong.com>
2013-09-18 10:40:04 -0500

    records converter

---
+++ lib/gsa/records_converter.rb
@@ -0,0 +1,55 @@
+module GSA
+  class RecordsConverter
+    extend XMLizer
+    extend Injector
+    extend Filer
+
+    def self.convert(args)
+      records         = args[:records]
+      file_name       = args[:file_name]
+      searchable      = args[:searchable]
+      datasource_name = args[:datasource_name]
+
+      # if there is only one record, convert to the expected array.
+      records = [records] if records.is_a? Hash
+
+      build_xml_file(file_name, layout(records, searchable, datasource_name))
+    end
+
+    #######
+    private
+    #######
+
+    def self.layout(records, searchable, datasource_name)
+      xml(
+        block(:gsafeed,
+              block(:header,
+                    block(:datasource, datasource_name) << 
+                    block(:feedtype, GSA::FEED_TYPE)
+              ) <<
+              block(:group, record_blocks(records, searchable))
+        )
+      )
+    end
+
+    def self.record_blocks(records, searchable)
+      inject_s(records) {|record| record_block(record, searchable)}
+    end
+
+    def self.record_block(record, searchable)
+      block(:record,
+        block(:metadata, record_metadata(record)) <<
+        block(:content, record_searchable(record, searchable)),
+        {:url => "#{GSA::DATASOURCE_BASE_URI}/#{record[GSA::DATASOURCE_UID.to_s]}", :mimetype => GSA::MIME_TYPE} # ABSTRACT URL STRING VALUE (id)
+      )
+    end
+
+    def self.record_searchable(record, searchable)
+      inject_s(searchable) {|search| " #{record[search.to_s]}"}
+    end
+
+    def self.record_metadata(record)
+      inject_s(record) {|key, value| tag(:meta, {:name => key, :content => value})}
+    end
+  end
+end
