9ea5d964d9561b6d2472b2e0684fef330ed3eaa1
daniel-jerome-long <daniel@danieljlong.com>
2013-10-10 08:49:43 -0500

    updated search_converter to leverage readable classes

--- lib/gsa/search_converter.rb
+++ lib/gsa/search_converter.rb
@@ -1,41 +1,18 @@
 module GSA
   class SearchConverter
-    extend Injector
 
     def self.convert(convertable)
-      parse result_sets_from results_param_in convertable
-    end
+      results     = GSA::ReadableResults.extract(convertable)
 
-    def self.results_param_in(convertable)
-      convertable[GSA::GOOGLE_SEARCH_PROTOCOL][GSA::RESULTS]
-    end
+      result_sets = GSA::ReadableResultSets.extract(results)
+      result_sets = GSA::ReadableResultSets.parse(result_sets)
+      
+      if results.include?(GSA::FACETS)
+        facets    = GSA::ReadableFacets.extract(results)
+        facets    = GSA::ReadableFacets.parse(facets)
+      end
 
-    def self.result_sets_from(results)
-      (results.keep_if {|key, value| key == GSA::RESULT })[GSA::RESULT]
-    end
-
-    def self.parse(result_sets)
-
-      # if there is only one result, convert to the expected array.
-      result_sets = [result_sets] if result_sets.is_a? Hash
-
-      inject_a(result_sets) {|set|
-        { 
-          :result_number          => set[GSA::RESULT_NUMBER], 
-          :url                    => set[GSA::URL],
-          :title                  => set[GSA::TITLE],
-          :metatags               => parse_metatags(set[GSA::METATAGS]),
-          :search_result_snippet  => set[GSA::SEARCH_RESULT_SNIPPET]
-        }
-      }
-    end
-
-    def self.parse_metatags(metatags)
-      inject_a(metatags) {|meta_pair| convert_meta_pair(meta_pair) }
-    end
-
-    def self.convert_meta_pair(meta_pair)
-      {:meta_name => meta_pair[GSA::META_NAME], :meta_value => meta_pair[GSA::META_VALUE]}
+      { :result_sets => result_sets, :facets => (facets || GSA::NO_RESULTS) }
     end
   end
 end
