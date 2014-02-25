e6e897e456bf2c35f3d0f15918bfbfb2e6a1b17f
daniel-jerome-long <daniel@danieljlong.com>
2014-02-25 10:34:03 -0600

    simplify uid extractor

--- lib/gsa/uid_extractor.rb
+++ lib/gsa/uid_extractor.rb
@@ -1,24 +1,11 @@
 module GSA
   class UIDExtractor
     def self.extract(search_results, uid)
-      iterate_results([], search_results, uid)
-    end
-
-    def self.iterate_results(uids, search_results, uid)
-      search_results.each {|result| get_uids_from_record(uids, result, uid)}
+      uids = []
+      search_results.map do |search_result|
+        search_result[:metatags].map {|m| uids << m[:meta_value] if m[:meta_name] == uid }
+      end
       uids
-    end
-
-    def self.get_uids_from_record(uids, record, uid)
-      record[:metatags].each {|tag| add_uid(uids, tag) if uid?(tag, uid)}
-    end
-
-    def self.add_uid(uids, meta_tag)
-      uids << meta_tag[:meta_value]
-    end
-
-    def self.uid?(tag, uid)
-      tag[:meta_name] == uid
     end
   end
 end
